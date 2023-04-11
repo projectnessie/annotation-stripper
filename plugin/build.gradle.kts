82ba6cb65be06f1ce4a359a505ebac5519328be1
Robert Stupp <snazy@snazy.de>
2023-04-11 18:03:53 +0200

    Gradle Plugin: Allow using Gradle configuration cache (#30)
    Gradle's configuration cache aims to avoid costly operations that happen during Gradle's configuration phase, which can be comparabily expensive.
    
    However, using the configuration cache adds some constraints on everything (plugins, custom tasks, build scripts) used in a Gradle build. For example, capturing some types (e.g. `Configuration`, `DependencySet`) is not possible, need to narrow to "more precise" types (like `FileCollection`).
    
    Also adds Java-Matrix CI for 11, 17, 19

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -56,6 +56,23 @@ kotlin { jvmToolchain(11) }
 
 tasks.named("pluginUnderTestMetadata") { dependsOn(tasks.named("processJandexIndex")) }
 
+tasks.withType(Test::class.java).configureEach {
+  jvmArgs(
+    "--add-exports=jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED",
+    "--add-exports=jdk.compiler/com.sun.tools.javac.file=ALL-UNNAMED",
+    "--add-exports=jdk.compiler/com.sun.tools.javac.main=ALL-UNNAMED",
+    "--add-exports=jdk.compiler/com.sun.tools.javac.model=ALL-UNNAMED",
+    "--add-exports=jdk.compiler/com.sun.tools.javac.parser=ALL-UNNAMED",
+    "--add-exports=jdk.compiler/com.sun.tools.javac.processing=ALL-UNNAMED",
+    "--add-exports=jdk.compiler/com.sun.tools.javac.tree=ALL-UNNAMED",
+    "--add-exports=jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED",
+    "--add-opens=jdk.compiler/com.sun.tools.javac.code=ALL-UNNAMED",
+    "--add-opens=jdk.compiler/com.sun.tools.javac.comp=ALL-UNNAMED",
+    "--add-opens=java.base/java.lang.invoke=ALL-UNNAMED",
+    "--add-opens=java.base/java.util=ALL-UNNAMED"
+  )
+}
+
 publishing {
   publications {
     withType(MavenPublication::class.java).configureEach {
